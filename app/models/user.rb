69086818a38bcb6ff14f98124f33d0bf10c87447
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-01 13:53:44 -0500

    Made user model more robust and add comments to explain usage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   include BCrypt
+  # make sure the user table has a password_hash column
 
   def password
     @password ||= Password.new(password_hash)
@@ -11,13 +12,14 @@ class User < ActiveRecord::Base
   end
 
   def self.create(params={})
-    @user = User.new(:email => params[:email], :name => params[:name])
+    @user = User.new(params)
     @user.password = params[:password]
     @user.save!
     @user
   end
 
   def self.authenticate(params)
+    # can be changed to find user by anything unique, e.g. email or username
     user = User.find_by_name(params[:name])
     (user && user.password == params[:password]) ? user : nil
   end
